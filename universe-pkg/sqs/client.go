f99a0786e71949081a2b4f4c2f5dc0162bce1599
Ivan Pavlov <i.pavlov.nix@gmail.com>
2025-08-29 00:42:49 +0300

    product-eventbus-pkg: add SQS client

---
+++ universe-pkg/sqs/client.go
@@ -0,0 +1,36 @@
+package sqsclient
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	sqsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+func NewClientSQS(
+	ctx context.Context,
+	sessionKey string,
+	socket string,
+	defaultRegion string,
+	accessKeyID string,
+	accessKeySecret string,
+) (*sqs.Client, error) {
+	config, err := sqsconfig.LoadDefaultConfig(ctx,
+		sqsconfig.WithRegion(defaultRegion),
+		sqsconfig.WithCredentialsProvider(
+			credentials.NewStaticCredentialsProvider(accessKeyID, accessKeySecret, sessionKey),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load SQS config: %w", err)
+	}
+
+	client := sqs.NewFromConfig(config, func(o *sqs.Options) {
+		o.BaseEndpoint = aws.String(socket)
+	})
+
+	return client, nil
+}
