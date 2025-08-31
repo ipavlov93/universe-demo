5c8ad66436e5c25fc79d8e92366ae71a2e3bd1a0
Ivan Pavlov <i.pavlov.nix@gmail.com>
2025-08-31 11:25:48 +0300

    notification-sv: edit zap logger encoding in factory

--- notification-sv/internal/infra/logger/zap/factory/zap_logger.go
+++ notification-sv/internal/infra/logger/zap/factory/zap_logger.go
@@ -2,6 +2,7 @@ package factory
 
 import (
 	"io"
+	"time"
 
 	"github.com/ipavlov93/universe-demo/universe-pkg/logger"
 	"go.uber.org/zap"
@@ -9,20 +10,22 @@ import (
 )
 
 // NewZapLogger constructs logger.
-// logger.ZapLogger write logs to the given io.Writer (zapcore.WriteSyncer) using JSON encoding with RFC3339 timestamps.
+// logger.ZapLogger write logs to the given io.Writer (zapcore.WriteSyncer) with RFC3339 timestamps.
 func NewZapLogger(w io.Writer, minLevel zapcore.Level) *logger.ZapLogger {
 	cfg := zap.NewProductionEncoderConfig()
-	cfg.EncodeTime = zapcore.RFC3339TimeEncoder
+
+	cfg.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.UTC().Format(time.RFC3339))
+	}
 
 	core := zapcore.NewCore(
-		zapcore.NewJSONEncoder(cfg),
+		zapcore.NewConsoleEncoder(cfg),
 		zapcore.AddSync(w),
 		minLevel,
 	)
 
 	options := []zap.Option{
 		zap.AddCaller(),
-		zap.AddCallerSkip(1),
 	}
 
 	return logger.NewWithCore(core, options...)
